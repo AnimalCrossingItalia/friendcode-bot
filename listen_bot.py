89796979a6a12bef2cfe8f8451a8eb28c0b54a54
Stefano Di Pasquale <stefaniscion@gmail.com>
2018-09-20 21:54:42 +0200

    Add files via upload

--- listen_bot.py
+++ listen_bot.py
@@ -10,19 +10,19 @@ def handle(msg):
 	command = msg['text']
 
 	#condizione permesso negato
-	if false: 
-        	bot.sendMessage(chat_id, "Mi dispiace! Questo e un bot privato! Accesso negato!")
+	if msg['chat']['type'] != 'group' and msg['chat']['type'] != 'supergroup': 
+        	bot.sendMessage(chat_id, "Questo bot va eseguito da un gruppo!")
         	exit(1)
 
 	#logging comandi
 	print 'Comando ricevuto: ' + command
 
 	#comandi
-	if command == '/ping':
+	if command.startswith('/ping'):
 		bot.sendMessage(chat_id, "pong!")
 	
-	elif command == '/chatid':
-		bot.sendMessage(chat_id, chat_id)
+	elif command.startswith('/msg'):
+		bot.sendMessage(chat_id, msg)
   	
 	elif command.startswith('/query'):
 		args = command.split(' ')
@@ -31,16 +31,16 @@ def handle(msg):
 		output = ' ' #inserire il risultato della query in "output"
 		bot.sendMessage(chat_id,output) #stampa output
 	
-	elif command.startswith('/insert'):
+	elif command.startswith('/delete'):
 		args = command.split(' ')
 		#inserire comando per insert, usare "args[1]" come parametro
 		#gestire eccezione per record non trovato
 		bot.sendMessage(chat_id,'Record eliminato') #conferma
 		
-	elif command.startswith('/delete'): 
+	elif command.startswith('/insert'): 
 		args = command.split(' ')
 		#inserire comando per delete, usare "args[1]" come parametro
-		#gestire eccezione per record già esistente
+		#gestire eccezione per record gia esistente
 		bot.sendMessage(chat_id,"Record creato") #conferma
     
 #Bot Token
