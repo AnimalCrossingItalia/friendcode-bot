da701432c2cbaf24228ac1c42b3f0b77fc2d5c0e
Stefano Di Pasquale <stefaniscion@gmail.com>
2018-09-20 20:52:30 +0200

    Update listen_bot.py

--- listen_bot.py
+++ listen_bot.py
@@ -11,20 +11,37 @@ def handle(msg):
 	chat_id = msg['chat']['id']
 	command = msg['text']
 
-	#permessi di utilizzo
-  	#if msg['from']['id'] != USER_ID:
+	#condizione permesso negato
 	if false: 
         	bot.sendMessage(chat_id, "Mi dispiace! Questo e un bot privato! Accesso negato!")
         	exit(1)
 
-	#log comandi
+	#logging comandi
 	print 'Comando ricevuto: ' + command
 
 	#comandi
 	if command == '/ping':
 		bot.sendMessage(chat_id, "pong!")
-  	elif command == '/list':
-    		bot.sendMessage(chat_id, "lista")
+  	
+	elif command.startswith('/query'):
+		args = command.split(' ')
+		#inserire comando per query, usare "args[1]" come parametro
+		#gestire eccezione per record non trovato
+		output = ' ' #inserire il risultato della query in "output"
+		bot.sendMessage(chat_id,output) #stampa parametro
+	
+	elif command.startswith('/insert'):
+		args = command.split(' ')
+		#inserire comando per insert, usare "args[1]" come parametro
+		#gestire eccezione per record non trovato
+		bot.sendMessage(chat_id,'Record eliminato') #conferma
+		
+		
+	elif command.startswith('/delete'): 
+		args = command.split(' ')
+		#inserire comando per delete, usare "args[1]" come parametro
+		#gestire eccezione per record già esistente
+		bot.sendMessage(chat_id,"Record creato") #conferma
     
 #Bot Token
 bot = telepot.Bot('TOKEN')
