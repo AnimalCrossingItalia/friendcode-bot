da2fb14b72d117be354fbc8eaa18bad721aedddd
Emiliano Sandri <emiliano.sandri@outlook.com>
2018-09-22 11:02:33 +0200

    Added exception handling in DataManager

--- Modules/DataManager.py
+++ Modules/DataManager.py
@@ -3,31 +3,48 @@ import psycopg2
 class DataManager:
 
     def __init__(self, databaseurl):
-        self.db = psycopg2.connect(databaseurl, sslmode='require')
+        self.db = databaseurl
 
     def addcode(self, chat, name, key, friendCode):
-        instring = """INSERT INTO data(user,chat,key,code) VALUES(%s,%s,%s,%s) RETURNING vendor_id;"""
-
-        cur = self.db.cursor()
-        cur.execute(instring, (name, chat, key, friendCode))
-        cur.Commit()
-        cur.close()
+        try:
+            db = psycopg2.connect(databaseurl, sslmode='require')
+            instring = """INSERT INTO data(user,chat,key,code) VALUES(%s,%s,%s,%s) RETURNING vendor_id;"""
+            cur = db.cursor()
+            cur.execute(instring, (name, chat, key, friendCode))
+            cur.Commit()
+        except:
+            print("Errore SQL!")
+        finally:
+            cur.close()
+            db.close()
 
     def removecode(self, chat, name, key):
-        delstring = """DELETE FROM data WHERE chat=%s AND name=%s AND key=%s"""
+        rows_deleted = 0
+        try:
+            db = psycopg2.connect(self.databaseurl, sslmode='require')
+            delstring = """DELETE FROM data WHERE chat=%s AND name=%s AND key=%s"""
+            cur = db.cursor()
+            cur.execute(delstring, (chat, name, key))
+            rows_deleted = cur.rowcount
+            cur.Commit()
+        except:
+            print("Errore SQL!")
+        finally:
+            cur.close()
+            db.close()
+            return rows_deleted
 
-        cur = self.db.cursor()
-        cur.execute(delstring, (chat, name, key))
-        rows_deleted = cur.rowcount
-        cur.Commit()
-        cur.close()
-        return rows_deleted
 
     def displayperson(self, chat, name):
-        qstring = """SELECT * FROM data WHERE chat=%s AND name=%s"""
-
-        cur = self.db.cursor()
-        cur.execute(qstring, (chat, name))
-        rows = cur.fetchall()
-        cur.close()
-        return rows
+        try:
+            db = psycopg2.connect(self.databaseurl, sslmode='require')
+            qstring = """SELECT * FROM data WHERE chat=%s AND name=%s"""
+            cur = db.cursor()
+            cur.execute(qstring, (chat, name))
+            rows = cur.fetchall()
+        except:
+            print("Errore SQL!")
+        finally:
+            cur.close()
+            db.close()
+            return rows
